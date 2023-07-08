42ad3f0e5d67713ca5c30cc3f26ed9e719bab8fb
Ritu Mahto <83583917+ritu220@users.noreply.github.com>
2023-07-08 11:01:23 +0530

    done with page 1 layout

--- j4d_admin_new/src/App.js
+++ j4d_admin_new/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-
 import './App.css';
-import Header from './Pages/JobFlow/Components/Header/Header';
-import Page1 from './Pages/JobFlow/Page1/Page1'
-import NavBar from './Pages/JobFlow/Components/MainNavBar/NavBar';
-import PlainInput from './Pages/JobFlow/Components/InputBoxes/Plain/TextFieldPlain';
-import ChipButtonGroup from './Pages/JobFlow/Components/InputBoxes/ChipGroup/Chip';
-import ChipGroup from './Pages/JobFlow/Components/InputBoxes/ChipGroup/ChipGroup';
-import Page2 from './Pages/JobFlow/Page2/Page2';
-import TwoThumbSlider from './Pages/JobFlow/Components/InputBoxes/Range/Range';
-import Page3 from './Pages/JobFlow/Page3/Page3';
-
+import Page1 from "./Pages/JobFlow/Page1/Page1";
 
 function App() {
-  
   return (
-    <div className="App">
-      <NavBar/>
-      <Page2/>
+    <div>
+      <Page1/>
     </div>
   );
 }
--- j4d_admin_new/src/Pages/JobFlow/Page1/Page1.jsx
+++ j4d_admin_new/src/Pages/JobFlow/Page1/Page1.jsx
@@ -1,105 +1,67 @@
-import './page.scss'
-import TopNavBar from '../Components/Navbar/TopNavBar'
-import Header from '../Components/Header/Header'
-import imgHeader1 from '../Assets/vectorP1.png'
-import PlainInput from '../Components/InputBoxes/Plain/TextFieldPlain'
-import Footer from '../Components/Footer/Footer'
-const Page1 = () => {
-    return (
-        <div className='JobFlowPage1'>
-            <div className='TopNav'>
-                <TopNavBar/>
-            </div>
-            <div className='Header'>
-                <Header
-                    Title = "Basic Information"
-                    vectorImg = {imgHeader1}
-                    page = "1"
-                />
-            </div>
-            <div className='Sec1'>
-                <div className='Sec1Left'>
-                    <div className='InputGroup'>
-                    <PlainInput
-                        id = "jobRole"
-                        type = "text"
-                        placeholder="Frontend Developer"
-                        label ="Job Role"
-                    />
-                    <div className='inputSetPage1'>
-                        <PlainInput
-                            id = "jobstate"
-                            type = "text"
-                            placeholder="Punjab"
-                            label ="State"
-                        />
-                        <PlainInput
-                            id = "jobCity"
-                            type = "text"
-                            placeholder="Ludhiana"
-                            label ="City"
-                        />
-                    </div>
-                    <PlainInput
-                        id = "ExpReq"
-                        type = "number"
-                        placeholder="0"
-                        label ="Experience Required"
-                    />
-                    </div>
-                    
-                </div>
-                <div className='Sec1Right'>
-                    <div className='InputGroup2'>
-                    <PlainInput
-                        id = "OpeningCount"
-                        type = "number"
-                        placeholder="0"
-                        label ="No of Job Openings"
-                    />
-                    <PlainInput
-                            id = "JobType"
-                            type = "text"
-                            placeholder="Full Time"
-                            label ="Job Type"
-                    />
-                    <PlainInput
-                        id = "Joblevel"
-                        type = "text"
-                        placeholder="Fresher.."
-                        label ="Job Level"
-                    />
-                    </div>
-                </div>
-            </div>
-            <div className='Sec2'>
+import React from 'react'
+import "./page.css";
 
-                <div className='Sec2Left'>
-                    <PlainInput
-                        id = "DueDateHire"
-                        type = "date"
-                        placeholder="12/01/2023"
-                        label ="Hiring Due Date"
-                    />
-                </div>
-                <div className='Sec2Right'>
-                    <PlainInput
-                        id = "JobField"
-                        type = "text"
-                        placeholder="Development"
-                        label ="Job Field"
-                    />
-                </div>
-            </div>
-            <div className='Footer'>
-                <Footer
-                    primary="Next"
-                    secondary1="Back"
-                    secondary2="Discard"
-                />
-            </div>
+export default function Page1() {
+  return (
+    <div>
+     
+      {/* navbar */}
+      
+      <div className="navbar">
+      
+          <div className="logo">
+              logo
+          </div>
+          <div className="navicons">
+              icons
+          </div>
+      </div>
+      
+      <div className="con">
+
+      
+      {/* mini navbar */}
+      <div className="mininavbar">
+
+      </div>
+
+      {/* main section  */}
+      <div className="main_section">
+        <div className="description">
+            description
         </div>
-    )
-}
+        <div className="image">
+           image
+        </div>
+      </div>
 
-export default Page1
+      {/* middle section */}
+
+      <div className="mid_section">
+        <div className="left">
+
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="Left">
+
+        </div>
+        <div className="Right">
+
+        </div>
+      </div>
+
+      <div className="end">
+
+      </div>
+      
+      
+    </div>
+    </div>
+    
+  )
+}
