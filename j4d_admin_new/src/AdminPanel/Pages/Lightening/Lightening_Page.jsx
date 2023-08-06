2405a8e60db71845cf5db64cf3253bb704a6f7d9
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2023-08-06 20:32:50 +0530

    .

--- j4d_admin_new/src/AdminPanel/Pages/Lightening/Lightening_Page.jsx
+++ j4d_admin_new/src/AdminPanel/Pages/Lightening/Lightening_Page.jsx
@@ -1,4 +1,40 @@
 const Lightening = () =>{
-    return (<></>);
+    const cards = data.map(item => {
+        return (
+            <JobCard
+                key={item.id}
+                item={item}
+                save = 'false'
+                logo = {logo}
+                time = '2'
+                Hired = '5'
+                page = 'LighteningPage'
+            />
+        )
+    }) 
+
+    return (
+    <>
+    <NavBar/>
+    <div className="Lightenning">
+        <div className="PageHeader">
+            <TopNav/>
+        </div>
+        <div className="LighteningBody">
+            <div className="topBody">
+            <div className='headings'>
+                    <h1 className='page-heading'style={{textAlign:"left"}} >Lightening</h1>
+                    <h1 className='page-sub-heading'>Supercharge Your Job Ads and Boost Your Reach to Top Talent!</h1>
+            </div>
+            </div>
+            <div className="BodyBottom">
+                <div className="cardlist">
+                    {cards}
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+    );
 }
 export default Lightening
