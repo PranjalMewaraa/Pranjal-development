b0779f54c3e8043f3bbd77eca0e26bd41f0802c4
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2023-08-12 15:13:58 +0530

    ..

--- j4d_admin_new/src/AdminPanel/Pages/Lightening/Lightening_pay/Lightening_pay.jsx
+++ j4d_admin_new/src/AdminPanel/Pages/Lightening/Lightening_pay/Lightening_pay.jsx
@@ -23,75 +23,75 @@ const Lg3 = () => {
               <div className="hires-sub-section">
                 <div>
                   <h1 className="page-heading">Location</h1>
-                  
+
                 </div>
 
                 <div>
-                <FiArrowLeft className="back-btn" />
-              </div>
+                  <FiArrowLeft fontSize='x-large' className="back-btn" />
+                </div>
 
               </div>
 
               <h1 className='page-sub-heading'>Select where you want your target audience from</h1>
-              <h1 className='page-sub-heading'><input type="checkbox"></input>Let j4d select your location according to your job profile </h1>
+              <h1 className='page-sub-heading'><input type="checkbox" id="xinp"></input>Let j4d select your location according to your job profile </h1>
               <div className="map">
-              <img  src={pic} alt="m"></img>
+                <img src={pic} alt="m"></img>
 
-              <h1 className='page-sub-heading'>Select Country</h1>
-              
-              <div className="duration-section">
-              <div className="drop-down-section">
-              <select className='selectbox' value="value-2" >
-                {options.map(item => {
-                  return (<option key={item.value} value={item.value}>{item.text}</option>);
-                })}
-              </select>
-            </div>
+                <h1 className='page-sub-heading'>Select Country</h1>
+
+                <div className="duration-section">
+                  <div className="drop-down-section">
+                    <select className='selectboxx' value="value-2" >
+                      {options.map(item => {
+                        return (<option key={item.value} value={item.value}>{item.text}</option>);
+                      })}
+                    </select>
+                  </div>
+                </div>
+
+
+                <div className="estimated">
+
+                  <div className="topic">
+                    Estimated results
+                  </div>
+
+                  <div className="border">
+
+                  </div>
+
+                  <div className="bottom">
+
+                    <div className="element">
+                      <span className="min">20k</span><span className="max">IMPRESSIONS</span>
+                    </div>
+
+                    <div className="element">
+                      <span className="min">12k</span><span className="max">CLICKS</span>
+                    </div>
+
+                    <div className="element">
+                      <span className="min">500+</span><span className="max">APPLICATIONS</span>
+                    </div>
+
+                  </div>
+
+                </div>
+
+
+
+
               </div>
-             
-
-            <div className="estimated">
-              
-               <div className="topic">
-               Estimated results
-               </div>
-
-               <div className="border">
-               
-               </div>
-
-               <div className="bottom">
-               
-               <div className="element">
-               <span className="min">20k</span><span className="max">IMPRESSIONS</span>
-               </div>
-
-               <div className="element">
-               <span className="min">12k</span><span className="max">CLICKS</span>
-               </div>
-
-               <div className="element">
-               <span className="min">500+</span><span className="max">APPLICATIONS</span>
-               </div>
-
-               </div>
-            
-            </div>
-
-             
-            
 
             </div>
 
-            </div>
-            
 
           </div>
           <div className="footer-section">
             <div className="footer-top">
               <div className="footer-text">
                 <p className="footerheading">
-                  <span className="span-text">₹6,400</span>/Week
+                  <span className="span-text" id="price">₹6,400</span>/Week
                 </p>
                 <p className="footersubhead">
                   Your daily budget might fluctuate but the weekly spends will
